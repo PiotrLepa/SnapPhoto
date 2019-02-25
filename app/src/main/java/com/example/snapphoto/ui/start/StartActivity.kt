31b5e459d3c7c63b7a82110e068e9cbca46adec1
piotr <piotr43a@gmail.com>
2019-02-25 12:53:25 +0100

    add back arrow

--- app/src/main/java/com/example/snapphoto/ui/start/StartActivity.kt
+++ app/src/main/java/com/example/snapphoto/ui/start/StartActivity.kt
@@ -1,13 +1,25 @@
 package com.example.snapphoto.ui.start
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.snapphoto.R
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val navController = findNavController(R.id.navHostFragment)
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.navHostFragment).navigateUp()
     }
 }
